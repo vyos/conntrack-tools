6dd62d2a2b8c06507c3e2eb5026a43e2deb3da6e
/C=EU/ST=EU/CN=Pablo Neira Ayuso/emailAddress=pablo@netfilter.org </C=EU/ST=EU/CN=Pablo Neira Ayuso/emailAddress=pablo@netfilter.org>
2008-01-15 13:40:35 +0000

    Add include/netlink.h and include/traffic_stats.h

---
+++ include/netlink.h
@@ -0,0 +1,21 @@
+#ifndef _NETLINK_H_
+#define _NETLINK_H_
+
+struct nf_conntrack;
+struct nfct_handle;
+
+int ignore_conntrack(struct nf_conntrack *ct);
+
+int nl_init_event_handler(void);
+
+int nl_init_dump_handler(void);
+
+void nl_resize_socket_buffer(struct nfct_handle *h);
+
+int nl_dump_conntrack_table(void);
+
+int nl_create_conntrack(struct nf_conntrack *ct);
+
+int nl_destroy_conntrack(struct nf_conntrack *ct);
+
+#endif
