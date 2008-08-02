6887cc03a9bc7df031b56ed670fa1db0165d5529
Pablo Neira Ayuso <pablo@netfilter.org>
2008-08-02 19:45:45 +0200

    script: fix broken if branches
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- doc/sync/primary-backup.sh
+++ doc/sync/primary-backup.sh
@@ -28,21 +28,27 @@ case "$1" in
     #
     $CONNTRACKD_BIN -C $CONNTRACKD_CONFIG -c
     if [ $? -eq 1 ]
+    then
         logger "ERROR: failed to invoke conntrackd -c"
+    fi
 
     #
     # flush the internal and the external caches
     #
     $CONNTRACKD_BIN -C $CONNTRACK_CONFIG -f
     if [ $? -eq 1 ]
+    then
     	logger "ERROR: failed to invoke conntrackd -f"
+    fi
 
     #
     # resynchronize my internal cache to the kernel table
     #
     $CONNTRACKD_BIN -C $CONNTRACKD_CONFIG -R
     if [ $? -eq 1 ]
+    then
     	logger "ERROR: failed to invoke conntrackd -R"
+    fi
     ;;
   backup)
     #
@@ -75,7 +81,9 @@ case "$1" in
     #
     $CONNTRACKD_BIN -C $CONNTRACKD_CONFIG -t
     if [ $? -eq 1 ]
+    then
     	logger "ERROR: failed to invoke conntrackd -t"
+    fi
 
     #
     # request resynchronization with master firewall replica (if any)
@@ -83,7 +91,9 @@ case "$1" in
     #
     $CONNTRACKD_BIN -C $CONNTRACKD_CONFIG -n
     if [ $? -eq 1 ]
+    then
     	logger "ERROR: failed to invoke conntrackd -n"
+    fi
     ;;
   *)
     logger "ERROR: unknown state transition"
