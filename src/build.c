91bf01ee31b754bb17f612ee13685ef0ffe9baa8
Samuel Gauthier <samuel.gauthier@6wind.com>
2009-05-12 23:09:29 +0200

    build: use uint16_t instead of uint32_t for uint16_t attributes
    Signed-off-by: Samuel Gauthier <samuel.gauthier@6wind.com>
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- src/build.c
+++ src/build.c
@@ -51,7 +51,7 @@ __build_u8(const struct nf_conntrack *ct, int a, struct nethdr *n, int b)
 static inline void 
 __build_u16(const struct nf_conntrack *ct, int a, struct nethdr *n, int b)
 {
-	uint32_t data = nfct_get_attr_u16(ct, a);
+	uint16_t data = nfct_get_attr_u16(ct, a);
 	data = htons(data);
 	addattr(n, b, &data, sizeof(uint16_t));
 }
