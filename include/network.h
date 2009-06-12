vyos/conntrack-tools

commit b808645ec71b7cc22cf5106b3d79625d07e6077c

sync: add support for DCCP state replication
This patch adds initial support for DCCP state replication.

Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

include/network.h
@@ -213,6 +213,7 @@ enum nta_attr {
 	NTA_DPAT_PORT,		/* uint16_t */
 	NTA_NAT_SEQ_ADJ = 16,	/* struct nta_attr_natseqadj */
 	NTA_STATE_SCTP,		/* struct nta_attr_sctp */
+	NTA_STATE_DCCP,		/* struct nta_attr_dccp */
 	NTA_MAX
 };
 
@@ -228,6 +229,10 @@ struct nta_attr_natseqadj {
 struct nta_attr_sctp {
 	uint8_t state;
 	uint32_t vtag_orig, vtag_repl;
+};
+
+struct nta_attr_dccp {
+	uint8_t state, role;
 };
 
 void build_payload(const struct nf_conntrack *ct, struct nethdr *n);

src/build.c
@@ -103,6 +103,16 @@ __build_sctp(const struct nf_conntrack *ct, struct nethdr *n)
 	addattr(n, NTA_STATE_SCTP, &data, sizeof(struct nta_attr_sctp));
 }
 
+static inline void 
+__build_dccp(const struct nf_conntrack *ct, struct nethdr *n)
+{
+	struct nta_attr_dccp data = {
+		.state = nfct_get_attr_u8(ct, ATTR_DCCP_STATE),
+		.role = nfct_get_attr_u8(ct, ATTR_DCCP_ROLE),
+	};
+	addattr(n, NTA_STATE_DCCP, &data, sizeof(struct nta_attr_dccp));
+}
+
 static enum nf_conntrack_attr nat_type[] =
 	{ ATTR_ORIG_NAT_SEQ_CORRECTION_POS, ATTR_ORIG_NAT_SEQ_OFFSET_BEFORE,
 	  ATTR_ORIG_NAT_SEQ_OFFSET_AFTER, ATTR_REPL_NAT_SEQ_CORRECTION_POS,
@@ -131,6 +141,8 @@ void build_payload(const struct nf_conntrack *ct, struct nethdr *n)
 		__build_u8(ct, ATTR_TCP_STATE, n, NTA_STATE_TCP);
 	else if (nfct_attr_is_set(ct, ATTR_SCTP_STATE))
 		__build_sctp(ct, n);
+	else if (nfct_attr_is_set(ct, ATTR_DCCP_STATE))
+		__build_dccp(ct, n);
 
 	if (!CONFIG(commit_timeout) && nfct_attr_is_set(ct, ATTR_TIMEOUT))
 		__build_u32(ct, ATTR_TIMEOUT, n, NTA_TIMEOUT);

src/parse.c
@@ -30,6 +30,7 @@ static void parse_u32(struct nf_conntrack *ct, int attr, void *data);
 static void parse_group(struct nf_conntrack *ct, int attr, void *data);
 static void parse_nat_seq_adj(struct nf_conntrack *ct, int attr, void *data);
 static void parse_sctp(struct nf_conntrack *ct, int attr, void *data);
+static void parse_dccp(struct nf_conntrack *ct, int attr, void *data);
 
 struct parser {
 	void 	(*parse)(struct nf_conntrack *ct, int attr, void *data);
@@ -126,6 +127,10 @@ static struct parser h[NTA_MAX] = {
 		.parse	= parse_sctp,
 		.size	= NTA_SIZE(sizeof(struct nta_attr_sctp)),
 	},
+	[NTA_STATE_DCCP] = {
+		.parse	= parse_dccp,
+		.size	= NTA_SIZE(sizeof(struct nta_attr_dccp)),
+	},
 };
 
 static void
@@ -180,6 +185,14 @@ parse_sctp(struct nf_conntrack *ct, int attr, void *data)
 	nfct_set_attr_u8(ct, ATTR_SCTP_STATE, this->state);
 	nfct_set_attr_u32(ct, ATTR_SCTP_VTAG_ORIG, ntohl(this->vtag_orig));
 	nfct_set_attr_u32(ct, ATTR_SCTP_VTAG_REPL, ntohl(this->vtag_repl));
+}
+
+static void
+parse_dccp(struct nf_conntrack *ct, int attr, void *data)
+{
+	struct nta_attr_dccp *this = data;
+	nfct_set_attr_u8(ct, ATTR_DCCP_STATE, this->state);
+	nfct_set_attr_u8(ct, ATTR_DCCP_ROLE, this->role);
 }
 
 int parse_payload(struct nf_conntrack *ct, struct nethdr *net, size_t remain)

doc/sync/alarm/conntrackd.conf
@@ -324,6 +324,7 @@ General {
 		Protocol Accept {
 			TCP
 			SCTP
+			DCCP
 		}
 
 		#

doc/sync/ftfw/conntrackd.conf
@@ -333,6 +333,7 @@ General {
 		Protocol Accept {
 			TCP
 			SCTP
+			DCCP
 		}
 
 		#

doc/sync/notrack/conntrackd.conf
@@ -314,6 +314,7 @@ General {
 		Protocol Accept {
 			TCP
 			SCTP
+			DCCP
 		}
 
 		#

commit d9c0564db6b3f3ecb196508458a91b03d45fadb2

build: use TLV format for SCTP/DCCP protocol information
In 400ae54438c4b85126f9fab0ae1dc067823b70f7, we added the SCTP
support by means of a structure that was encapsulated in an
TLV attribute. However, this structure didn't handle alignment
and endianess issues appropriately. Similar problem was
introduced in b808645ec71b7cc22cf5106b3d79625d07e6077c along
with the DCCP support.

This patch moves every field of this structure to independent
attributes. I decided not to use nesting to make building and
parsing more simple.

Using TLV is a good idea, specially for DCCP and SCTP that are
under development and that may include new fields and obsolete
them in the future.

Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

include/network.h
@@ -199,7 +199,7 @@ enum nta_attr {
 	NTA_IPV6,		/* struct nfct_attr_grp_ipv6 */
 	NTA_L4PROTO,		/* uint8_t */
 	NTA_PORT,		/* struct nfct_attr_grp_port */
-	NTA_STATE_TCP = 4,	/* uint8_t */
+	NTA_TCP_STATE = 4,	/* uint8_t */
 	NTA_STATUS,		/* uint32_t */
 	NTA_TIMEOUT,		/* uint32_t */
 	NTA_MARK,		/* uint32_t */
@@ -212,8 +212,11 @@ enum nta_attr {
 	NTA_SPAT_PORT,		/* uint16_t */
 	NTA_DPAT_PORT,		/* uint16_t */
 	NTA_NAT_SEQ_ADJ = 16,	/* struct nta_attr_natseqadj */
-	NTA_STATE_SCTP,		/* struct nta_attr_sctp */
-	NTA_STATE_DCCP,		/* struct nta_attr_dccp */
+	NTA_SCTP_STATE,		/* uint8_t */
+	NTA_SCTP_VTAG_ORIG,	/* uint32_t */
+	NTA_SCTP_VTAG_REPL,	/* uint32_t */
+	NTA_DCCP_STATE = 20,	/* uint8_t */
+	NTA_DCCP_ROLE,		/* uint8_t */
 	NTA_MAX
 };
 
@@ -224,15 +227,6 @@ struct nta_attr_natseqadj {
 	uint32_t repl_seq_correction_pos;
 	uint32_t repl_seq_offset_before;
 	uint32_t repl_seq_offset_after;
-};
-
-struct nta_attr_sctp {
-	uint8_t state;
-	uint32_t vtag_orig, vtag_repl;
-};
-
-struct nta_attr_dccp {
-	uint8_t state, role;
 };
 
 void build_payload(const struct nf_conntrack *ct, struct nethdr *n);

src/build.c
@@ -92,27 +92,6 @@ __build_natseqadj(const struct nf_conntrack *ct, struct nethdr *n)
 	addattr(n, NTA_NAT_SEQ_ADJ, &data, sizeof(struct nta_attr_natseqadj));
 }
 
-static inline void 
-__build_sctp(const struct nf_conntrack *ct, struct nethdr *n)
-{
-	struct nta_attr_sctp data = {
-		.state = nfct_get_attr_u8(ct, ATTR_SCTP_STATE),
-		.vtag_orig = htonl(nfct_get_attr_u32(ct, ATTR_SCTP_VTAG_ORIG)),
-		.vtag_repl = htonl(nfct_get_attr_u32(ct, ATTR_SCTP_VTAG_REPL)),
-	};
-	addattr(n, NTA_STATE_SCTP, &data, sizeof(struct nta_attr_sctp));
-}
-
-static inline void 
-__build_dccp(const struct nf_conntrack *ct, struct nethdr *n)
-{
-	struct nta_attr_dccp data = {
-		.state = nfct_get_attr_u8(ct, ATTR_DCCP_STATE),
-		.role = nfct_get_attr_u8(ct, ATTR_DCCP_ROLE),
-	};
-	addattr(n, NTA_STATE_DCCP, &data, sizeof(struct nta_attr_dccp));
-}
-
 static enum nf_conntrack_attr nat_type[] =
 	{ ATTR_ORIG_NAT_SEQ_CORRECTION_POS, ATTR_ORIG_NAT_SEQ_OFFSET_BEFORE,
 	  ATTR_ORIG_NAT_SEQ_OFFSET_AFTER, ATTR_REPL_NAT_SEQ_CORRECTION_POS,
@@ -138,11 +117,15 @@ void build_payload(const struct nf_conntrack *ct, struct nethdr *n)
 	__build_u32(ct, ATTR_STATUS, n, NTA_STATUS); 
 
 	if (nfct_attr_is_set(ct, ATTR_TCP_STATE))
-		__build_u8(ct, ATTR_TCP_STATE, n, NTA_STATE_TCP);
-	else if (nfct_attr_is_set(ct, ATTR_SCTP_STATE))
-		__build_sctp(ct, n);
-	else if (nfct_attr_is_set(ct, ATTR_DCCP_STATE))
-		__build_dccp(ct, n);
+		__build_u8(ct, ATTR_TCP_STATE, n, NTA_TCP_STATE);
+	else if (nfct_attr_is_set(ct, ATTR_SCTP_STATE)) {
+		__build_u8(ct, ATTR_SCTP_STATE, n, NTA_SCTP_STATE);
+		__build_u32(ct, ATTR_SCTP_VTAG_ORIG, n, NTA_SCTP_VTAG_ORIG);
+		__build_u32(ct, ATTR_SCTP_VTAG_REPL, n, NTA_SCTP_VTAG_REPL);
+	} else if (nfct_attr_is_set(ct, ATTR_DCCP_STATE)) {
+		__build_u8(ct, ATTR_DCCP_STATE, n, NTA_DCCP_STATE);
+		__build_u8(ct, ATTR_DCCP_ROLE, n, NTA_DCCP_ROLE);
+	}
 
 	if (!CONFIG(commit_timeout) && nfct_attr_is_set(ct, ATTR_TIMEOUT))
 		__build_u32(ct, ATTR_TIMEOUT, n, NTA_TIMEOUT);

src/parse.c
@@ -29,8 +29,6 @@ static void parse_u16(struct nf_conntrack *ct, int attr, void *data);
 static void parse_u32(struct nf_conntrack *ct, int attr, void *data);
 static void parse_group(struct nf_conntrack *ct, int attr, void *data);
 static void parse_nat_seq_adj(struct nf_conntrack *ct, int attr, void *data);
-static void parse_sctp(struct nf_conntrack *ct, int attr, void *data);
-static void parse_dccp(struct nf_conntrack *ct, int attr, void *data);
 
 struct parser {
 	void 	(*parse)(struct nf_conntrack *ct, int attr, void *data);
@@ -59,7 +57,7 @@ static struct parser h[NTA_MAX] = {
 		.attr	= ATTR_L4PROTO,
 		.size	= NTA_SIZE(sizeof(uint8_t)),
 	},
-	[NTA_STATE_TCP] = {
+	[NTA_TCP_STATE] = {
 		.parse	= parse_u8,
 		.attr	= ATTR_TCP_STATE,
 		.size	= NTA_SIZE(sizeof(uint8_t)),
@@ -123,13 +121,30 @@ static struct parser h[NTA_MAX] = {
 		.parse	= parse_nat_seq_adj,
 		.size	= NTA_SIZE(sizeof(struct nta_attr_natseqadj)),
 	},
-	[NTA_STATE_SCTP] = {
-		.parse	= parse_sctp,
-		.size	= NTA_SIZE(sizeof(struct nta_attr_sctp)),
+	[NTA_SCTP_STATE] = {
+		.parse	= parse_u8,
+		.attr	= ATTR_SCTP_STATE,
+		.size	= NTA_SIZE(sizeof(uint8_t)),
 	},
-	[NTA_STATE_DCCP] = {
-		.parse	= parse_dccp,
-		.size	= NTA_SIZE(sizeof(struct nta_attr_dccp)),
+	[NTA_SCTP_VTAG_ORIG] = {
+		.parse	= parse_u32,
+		.attr	= ATTR_SCTP_VTAG_ORIG,
+		.size	= NTA_SIZE(sizeof(uint32_t)),
+	},
+	[NTA_SCTP_VTAG_REPL] = {
+		.parse	= parse_u32,
+		.attr	= ATTR_SCTP_VTAG_REPL,
+		.size	= NTA_SIZE(sizeof(uint32_t)),
+	},
+	[NTA_DCCP_STATE] = {
+		.parse	= parse_u8,
+		.attr	= ATTR_DCCP_STATE,
+		.size	= NTA_SIZE(sizeof(uint8_t)),
+	},
+	[NTA_DCCP_ROLE] = {
+		.parse	= parse_u8,
+		.attr	= ATTR_DCCP_ROLE,
+		.size	= NTA_SIZE(sizeof(uint8_t)),
 	},
 };
 
@@ -176,23 +191,6 @@ parse_nat_seq_adj(struct nf_conntrack *ct, int attr, void *data)
 			  ntohl(this->orig_seq_correction_pos));
 	nfct_set_attr_u32(ct, ATTR_REPL_NAT_SEQ_OFFSET_AFTER, 
 			  ntohl(this->orig_seq_correction_pos));
-}
-
-static void
-parse_sctp(struct nf_conntrack *ct, int attr, void *data)
-{
-	struct nta_attr_sctp *this = data;
-	nfct_set_attr_u8(ct, ATTR_SCTP_STATE, this->state);
-	nfct_set_attr_u32(ct, ATTR_SCTP_VTAG_ORIG, ntohl(this->vtag_orig));
-	nfct_set_attr_u32(ct, ATTR_SCTP_VTAG_REPL, ntohl(this->vtag_repl));
-}
-
-static void
-parse_dccp(struct nf_conntrack *ct, int attr, void *data)
-{
-	struct nta_attr_dccp *this = data;
-	nfct_set_attr_u8(ct, ATTR_DCCP_STATE, this->state);
-	nfct_set_attr_u8(ct, ATTR_DCCP_ROLE, this->role);
 }
 
 int parse_payload(struct nf_conntrack *ct, struct nethdr *net, size_t remain)