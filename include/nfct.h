e96bfcd1b9f79cb3642b365a584359b1672f2ac0
Pablo Neira Ayuso <pablo@netfilter.org>
2012-05-15 01:51:29 +0200

    conntrackd: add cthelper infrastructure (+ example FTP helper)
    This patch adds the user-space helper infrastructure. It also
    contains the implementation of the FTP helper in user-space.
    
    There's one example file that you can use to configure conntrackd
    as user-space connection tracking helper under:
    
     doc/helper/conntrackd.conf
    
    Signed-off-by: Pablo Neira Ayuso <pablo@netfilter.org>

--- include/nfct.h
+++ include/nfct.h
@@ -4,6 +4,7 @@
 enum {
 	NFCT_SUBSYS_NONE = 0,
 	NFCT_SUBSYS_TIMEOUT,
+	NFCT_SUBSYS_HELPER,
 	NFCT_SUBSYS_VERSION,
 	NFCT_SUBSYS_HELP,
 };
@@ -15,6 +16,7 @@ enum {
 	NFCT_CMD_DELETE,
 	NFCT_CMD_GET,
 	NFCT_CMD_FLUSH,
+	NFCT_CMD_DISABLE,
 };
 
 void nfct_perror(const char *msg);
@@ -25,5 +27,13 @@ int nfct_cmd_timeout_add(int argc, char *argv[]);
 int nfct_cmd_timeout_delete(int argc, char *argv[]);
 int nfct_cmd_timeout_get(int argc, char *argv[]);
 int nfct_cmd_timeout_flush(int argc, char *argv[]);
+
+int nfct_cmd_helper_parse_params(int argc, char *argv[]);
+int nfct_cmd_helper_list(int argc, char *argv[]);
+int nfct_cmd_helper_add(int argc, char *argv[]);
+int nfct_cmd_helper_delete(int argc, char *argv[]);
+int nfct_cmd_helper_get(int argc, char *argv[]);
+int nfct_cmd_helper_flush(int argc, char *argv[]);
+int nfct_cmd_helper_disable(int argc, char *argv[]);
 
 #endif
