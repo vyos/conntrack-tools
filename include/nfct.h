ea27bb406e3d8fe9466ba274af38e6f540ff5bfc
Alexander Wirt <formorer@debian.org>
2012-06-03 08:49:55 +0200

    Imported Upstream version 1.2.1

---
+++ include/nfct.h
@@ -0,0 +1,29 @@
+#ifndef _NFCT_H_
+#define _NFCT_H_
+
+enum {
+	NFCT_SUBSYS_NONE = 0,
+	NFCT_SUBSYS_TIMEOUT,
+	NFCT_SUBSYS_VERSION,
+	NFCT_SUBSYS_HELP,
+};
+
+enum {
+	NFCT_CMD_NONE = 0,
+	NFCT_CMD_LIST,
+	NFCT_CMD_ADD,
+	NFCT_CMD_DELETE,
+	NFCT_CMD_GET,
+	NFCT_CMD_FLUSH,
+};
+
+void nfct_perror(const char *msg);
+
+int nfct_cmd_timeout_parse_params(int argc, char *argv[]);
+int nfct_cmd_timeout_list(int argc, char *argv[]);
+int nfct_cmd_timeout_add(int argc, char *argv[]);
+int nfct_cmd_timeout_delete(int argc, char *argv[]);
+int nfct_cmd_timeout_get(int argc, char *argv[]);
+int nfct_cmd_timeout_flush(int argc, char *argv[]);
+
+#endif
